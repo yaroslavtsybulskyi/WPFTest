06d2304330a70f9da05d03ac4ea4da63ccccdba0
yaroslavtsybulskyi <tsybulskyi@outlook.com>
2023-02-25 13:42:38 +0200

    HW1-2

---
+++ Logistic.ConsoleClient/Cargo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace Logistic.ConsoleClient
+{
+	public class Cargo
+	{
+		public double Volume { get; set; }
+		public int Weight { get; set; }
+		public string Code { get; set; }
+		public bool Free { get; set; }
+
+		public Cargo()
+		{
+			this.Volume = 0d;
+			this.Weight = 0;
+			this.Code = "0000";
+			this.Free = true;
+		}
+
+		public Cargo(double volume, int weight, string code)
+		{
+			this.Volume = volume;
+			this.Weight = weight;
+			this.Code = code;
+			this.Free = false;
+		}
+
+		//returns information about cargo
+		public string GetInformation()
+		{
+			string textToReturn = $"Volume of the cargo: {this.Volume} m3 \nWeight of the cargo: {this.Weight} kg \nCargo Code: {this.Code}";
+			return textToReturn;
+		}
+	}
+}
+
---
+++ Logistic.ConsoleClient/Vehicle.cs
@@ -0,0 +1,186 @@
+﻿using System;
+namespace Logistic.ConsoleClient
+{
+	public class Vehicle
+	{
+
+		public const double CONVERSIONRATEKGPND = 2.2;
+
+        public VehicleType Type { get; set; }
+        public string Number { get; set; }
+        public int MaxCargoWeightKg { get; set; }
+        public double MaxCargoWeightPnd { get; set; }
+        public double MaxCargoVolume { get; set; }
+        public Cargo[] Cargos { get; set; }
+		public int CargoWeightLeftKg { get; set; }
+		public double CargoVolumeLeft { get; set; }
+
+        public enum VehicleType
+        {
+            Car,
+            Ship,
+            Plane,
+            Train
+        }
+
+		public enum WeightUnit
+		{
+			Kilograms,
+			Pounds
+		}
+
+		public Vehicle(VehicleType type, int maxCargoWeightKg, double maxCargoVolume)
+		{
+			this.Type = type;
+			this.Number = "AA11";
+			this.MaxCargoWeightKg = maxCargoWeightKg;
+			this.MaxCargoWeightPnd = CONVERSIONRATEKGPND * maxCargoWeightKg;
+			this.MaxCargoVolume = maxCargoVolume;
+			this.CargoVolumeLeft = this.MaxCargoVolume;
+			this.CargoWeightLeftKg = this.MaxCargoWeightKg;
+			this.Cargos = new Cargo[100];
+			for (int i = 0; i < this.Cargos.Length; i++)
+			{
+				this.Cargos[i] = new Cargo();
+			}
+
+		}
+
+		//Gets available volume for cargo
+		public string GetCargoVolumeLeft()
+		{
+			double volumeUsed = 0d;
+			foreach (Cargo i in Cargos)
+			{
+				volumeUsed += i.Volume;
+			}
+
+			double volumeLeft = this.MaxCargoVolume - volumeUsed;
+			return $"Volume left: {volumeLeft}";
+		}
+
+		//Gets available weight unit of cargo
+		public string GetCargoWeightLeft(WeightUnit weightUnit)
+		{
+			if (weightUnit == WeightUnit.Kilograms)
+			{
+				int kgUsed = 0;
+				foreach (Cargo i in Cargos)
+				{
+					kgUsed += i.Weight;
+				}
+
+				int weightLeftKg = this.MaxCargoWeightKg - kgUsed;
+				return $"Weight Left: {weightLeftKg} kg";
+
+			}
+			else
+			{
+				int usedInKg = 0;
+				double pndUsed = 0d;
+				foreach (Cargo i in Cargos)
+				{
+					usedInKg += i.Weight;
+				}
+
+				pndUsed = CONVERSIONRATEKGPND * usedInKg;
+
+				double weightLeftPnd = this.MaxCargoWeightPnd - pndUsed;
+				return $"Weight Left: {weightLeftPnd} pnd";
+
+            }
+
+		}
+
+		//Gets information about Vehicle
+		public string GetInformation()
+		{
+				int numberOfCargo = 0;
+				double totalCargoVolume = 0;
+				int totalCargoWeight = 0;
+
+				foreach (Cargo i in Cargos)
+				{
+				if (i.Free == false)
+				{
+					numberOfCargo += 1;
+				}
+				
+				}
+
+
+				foreach (Cargo i in Cargos)
+				{
+					totalCargoVolume += i.Volume;
+					totalCargoWeight += i.Weight;
+					
+				}
+
+				if(totalCargoVolume > 0d && totalCargoWeight > 0)
+				{
+
+					string textToReturn = $"Vehicle Type: {this.Type}\nNumber: {this.Number}\nMaxCargoWeightInKg: {this.MaxCargoWeightKg} kg" +
+					$"\nMaxCargoWeightInPnd: {this.MaxCargoWeightPnd} pnd \nMaxCargoVolume: {this.MaxCargoVolume} m3 \nNumber of Cargo: {numberOfCargo} " +
+					$"\nTotal Cargo Volume: {totalCargoVolume} m3 \nTotal Cargo Weight: {totalCargoWeight} kg";
+
+				return textToReturn;
+				}
+			else
+				{
+					string textToReturn = $"Vehicle Type: {this.Type}\nNumber: {this.Number}\nMaxCargoWeightInKg: {this.MaxCargoWeightKg} kg " +
+                    $"\nMaxCargoWeightInPnd: {this.MaxCargoWeightPnd} pnd \nMaxCargoVolume: {this.MaxCargoVolume} m3 \nNo cargo loaded";
+                return textToReturn;
+				}
+		}
+
+		//Loads cargo to vehicle
+		public void LoadCargo(Cargo cargo)
+		{
+			try
+			{
+				int cargoWeightAfterLoading = this.CargoWeightLeftKg - cargo.Weight;
+
+				if (cargoWeightAfterLoading < 0)
+				{
+					throw new Overweight();
+				}
+				else
+				{
+                    this.CargoWeightLeftKg -= cargo.Weight;
+                }
+
+				
+				double cargoVolumeAfterLoading = this.CargoVolumeLeft - cargo.Volume;
+
+				if (cargoVolumeAfterLoading < 0)
+				{
+					throw new MoreVolumeNeeded();
+				}
+				else
+				{
+                    this.CargoVolumeLeft -= cargo.Volume;
+                }
+
+                for (int i = 0; i < this.Cargos.Length; i++)
+                {
+                    if (this.Cargos[i].Free == true)
+                    {
+                        this.Cargos[i] = cargo;
+						this.Cargos[i].Free = false;
+                        break;
+                    }
+                }
+            }
+			catch (MoreVolumeNeeded)
+			{
+				Console.WriteLine($"Cargo is {cargo.Volume} m3 and {cargo.Weight} kg. More volume needed");
+			}
+			catch (Overweight)
+			{
+				Console.WriteLine($"Cargo is {cargo.Volume} m3 and {cargo.Weight} kg. Vehicle is overweight");
+			}
+		}
+
+	}
+}
+
