84cfdb3817bb4b6e742727f99c6ddef45f04f68a
yaroslavtsybulskyi <tsybulskyi@outlook.com>
2023-04-15 19:50:46 +0300

    comments fix

--- HW5/Script_1_Creation_of_DB_and_tables.sql
+++ HW5/Script_1_Creation_of_DB_and_tables.sql
@@ -7,36 +7,51 @@ GO
 USE logistics;
 GO
 
-CREATE TABLE dbo.Vehicles (
-    Id INT PRIMARY KEY,
-    Type VARCHAR(50),
-    Number VARCHAR(50),
-    MaxCargoWeightKg INT,
-    MaxCargoWeightPd FLOAT,
-    MaxCargoVolume FLOAT
-);
+IF DB_ID() > 0
+BEGIN
+    IF OBJECT_ID('dbo.Vehicles', 'U') IS NULL
+    BEGIN
+        CREATE TABLE dbo.Vehicles (
+            Id INT PRIMARY KEY,
+            Type NVARCHAR(50),
+            Number NVARCHAR(50),
+            MaxCargoWeightKg INT,
+            MaxCargoWeightPd FLOAT,
+            MaxCargoVolume FLOAT
+        );
+    END
 
-CREATE TABLE dbo.Warehouses (
-    Id INT PRIMARY KEY
-);
+    IF OBJECT_ID('dbo.Warehouses', 'U') IS NULL
+    BEGIN
+        CREATE TABLE dbo.Warehouses (
+            Id INT PRIMARY KEY
+        );
+    END
 
-CREATE TABLE dbo.Invoices (
-    Id UNIQUEIDENTIFIER PRIMARY KEY,
-    RecipientAddress VARCHAR(100),
-    SenderAddress VARCHAR(100),
-    RecipientPhoneNumber VARCHAR(20),
-    SenderPhoneNumber VARCHAR(20)
-);
+    IF OBJECT_ID('dbo.Invoices', 'U') IS NULL
+    BEGIN
+        CREATE TABLE dbo.Invoices (
+            Id UNIQUEIDENTIFIER PRIMARY KEY,
+            RecipientAddress NVARCHAR(100),
+            SenderAddress NVARCHAR(100),
+            RecipientPhoneNumber NVARCHAR(20),
+            SenderPhoneNumber NVARCHAR(20)
+        );
+    END
 
-CREATE TABLE dbo.Cargos (
-    Id UNIQUEIDENTIFIER PRIMARY KEY,
-    Code VARCHAR(50),
-    Weight INT,
-    Volume FLOAT,
-    VehicleId INT NULL REFERENCES dbo.Vehicles(Id),
-    WarehouseId INT NULL REFERENCES dbo.Warehouses(Id),
-    InvoiceId UNIQUEIDENTIFIER FOREIGN KEY REFERENCES dbo.Invoices(Id),
-    CONSTRAINT FK_Cargos_Vehicles FOREIGN KEY (VehicleId) REFERENCES dbo.Vehicles(Id),
-    CONSTRAINT FK_Cargos_Warehouses FOREIGN KEY (WarehouseId) REFERENCES dbo.Warehouses(Id),
-    CONSTRAINT FK_Cargos_Invoices FOREIGN KEY (InvoiceId) REFERENCES dbo.Invoices(Id)
-);
+    IF OBJECT_ID('dbo.Cargos', 'U') IS NULL
+    BEGIN
+        CREATE TABLE dbo.Cargos (
+            Id UNIQUEIDENTIFIER PRIMARY KEY,
+            Code NVARCHAR(50),
+            Weight INT,
+            Volume FLOAT,
+            VehicleId INT NULL,
+            WarehouseId INT NULL,
+            InvoiceId UNIQUEIDENTIFIER NULL,
+            CONSTRAINT FK_Cargos_Vehicles FOREIGN KEY (VehicleId) REFERENCES dbo.Vehicles(Id),
+            CONSTRAINT FK_Cargos_Warehouses FOREIGN KEY (WarehouseId) REFERENCES dbo.Warehouses(Id),
+            CONSTRAINT FK_Cargos_Invoices FOREIGN KEY (InvoiceId) REFERENCES dbo.Invoices(Id)
+        );
+    END
+END
